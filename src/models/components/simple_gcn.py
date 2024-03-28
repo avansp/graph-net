eba1063b01b5a0bbfb39081eac8476294cb408ed
avansp <avan.sp@gmail.com>
2024-03-28 12:54:46 +0000

    Refactored generic cora into cora_gcn

--- src/models/components/simple_gcn.py
+++ src/models/components/simple_gcn.py
@@ -2,29 +2,15 @@ from torch import nn
 import torch_geometric.nn as geom_nn
 
 
-class GNNModel(nn.Module):
-    """A graph network model.
-
-    There are three types of graph net defined in this class during initialisation:
-    * layer_name='GCN' : Graph Convolutional Network
-    * layer_name='GAT' : Graph Attention Network
-    * layer_name='GraphConv' : Graph Neural Network
-    """
-    __GNN_BY_LAYER_NAME = {
-        "GCN": geom_nn.GCNConv,
-        "GAT": geom_nn.GATConv,
-        "GraphConv": geom_nn.GraphConv
-    }
-
+class GCNModel(nn.Module):
+    """A graph network model."""
     def __init__(
         self,
         c_in: int,
         c_hidden: int,
         c_out: int,
         num_layers: int = 2,
-        layer_name: str = "GCN",
-        dp_rate: str = 0.1,
-        **kwargs
+        dp_rate: float = 0.1
     ) -> None:
         """Initialise graph network model.
 
@@ -32,25 +18,24 @@ class GNNModel(nn.Module):
         :param c_hidden: Dimension of the hidden features.
         :param c_out: Dimension of the output features.
         :param num_layers: Number of hidden graph layers.
-        :param dp_rate: Dropout rate to apply throghout the network.
+        :param dp_rate: Dropout rate to apply throughout the network.
         :param kwargs: Additional parameters for the graph layer, e.g. number of heads for GAT.
         """
         super().__init__()
-        gnn_layer = self.__GNN_BY_LAYER_NAME[layer_name]
 
         # create intermediate layers from 0 ... (num_layers-1)
         layers = []
         in_channels, out_channels = c_in, c_hidden
         for _ in range(num_layers-1):
             layers += [
-                gnn_layer(in_channels=in_channels, out_channels=out_channels, **kwargs),
+                geom_nn.GCNConv(in_channels=in_channels, out_channels=out_channels),
                 nn.ReLU(inplace=True),
                 nn.Dropout(dp_rate)
             ]
             in_channels = c_hidden
 
         # add the last layer
-        layers += [gnn_layer(in_channels=in_channels, out_channels=out_channels, **kwargs)]
+        layers += [geom_nn.GCNConv(in_channels=in_channels, out_channels=c_out)]
 
         # final module
         self.layers = nn.ModuleList(layers)
